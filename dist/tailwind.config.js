5280ca6e80774f1dfe0bde89bd4e774242102d4c
Yor <34521238+esrey@users.noreply.github.com>
2022-06-02 10:40:03 -0700

    sending to github

---
+++ dist/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+
+    screens: {
+      'tablet': '640px',
+
+
+      'laptop': '1024px',
+
+
+      'desktop': '1280px',
+    
+    },
+  }
